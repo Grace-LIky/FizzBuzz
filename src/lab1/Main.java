d5dedaef13ffbc59701663e037be8cf1c1fa5937
Grace Li <graceli@Mac.rally.ca>
2026-09-14 23:31:24 -0400

    complete Main access modifier exercise

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -2,10 +2,13 @@ package lab1;
 
 public class Main {
     static void main(String[] args) {
+    //Task 3.1
+
         FizzBuzz.doFizzBuzz(5);
         // doFizzBuzz in FizzBuzz.java is public.
         // Such that we could call it here.
 
+    //Task 3.2
         // FizzBuzz.main();
         // The line above does not work.
         // public static void main(String[] args) requires one argument
@@ -13,7 +16,6 @@ public class Main {
         FizzBuzz.main(args);
         // or FizzBuzz.main(new String[0]);
 
-
-
     }
 }
+
